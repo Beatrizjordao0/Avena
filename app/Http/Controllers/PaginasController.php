a52eec20d1542f36e4bf4690e3e52233c0d56dc2
bibiperigosa <beatrizjordao15@gmail.com>
2025-11-19 20:30:33 -0300

    avena-site

---
+++ app/Http/Controllers/PaginasController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaginasController extends Controller
+{
+
+     public function index() {
+        return view('index');
+    }
+
+    public function login() { 
+        return view("login");
+    }
+
+    public function email() {
+        return view('email');
+    }
+
+    public function password() {
+        return view('password');
+    }
+
+    public function register() {
+        return view('register');
+    }
+
+    public function schedule() {
+        return view('schedule');
+    }
+
+}
+

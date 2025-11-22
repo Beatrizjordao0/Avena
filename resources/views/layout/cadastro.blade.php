1e44a17342dfb612b4ae5ffac03fb32745231c90
alysoncdps <coutinhoalyson138@gmail.com>
2025-11-22 21:55:58 +0000

    ALTERAÇÃO NO CADASTRO.CSS E CADASTRO.BLADE.PHP

--- resources/views/layout/cadastro.blade.php
+++ resources/views/layout/cadastro.blade.php
@@ -6,16 +6,18 @@
     <title>Avena | Cadastro</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link rel="stylesheet" href="/css/cadastro.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Goldman:wght@400;700&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Playwrite+DE+SAS:wght@100..400&family=Poppins:wght@100;400;700&display=swap" rel="stylesheet">
     <style>
-
     </style>
 </head>
-<body>
+<body class="nunita-bold">
             <!-- Logo do cabeçalho -->
-        <div class="logo">
-            <img src='/img/logo-avena-removebg-preview-dark' alt="Imagem da logo da Avena">
-            <span>Avena</span>
+        <div class="logo nunita-bold">
+            <img src='/img/logo-avena-removebg-preview-dark.png' alt="Imagem da logo da Avena">
+            <span class="poppins-bold text-name">Avena</span>
         </div>
             <!-- Parte superior do cadastro -->
         <div class="left-side-container">
@@ -24,34 +26,27 @@
                 <a href="#" class="help-link">Ajuda?</a>
             </div>
 
-            <div class="form-container">
-                <div class="form-heading">
+            <div class=" form-container ">
+                <div class="formform-heading">
                     @yield("form-heading")
                 </div>
-                <form action="{{ route('cadastro.salvarEtapa') }}" method="POST">
+                <form class="form-row" action="{{ route('cadastro.salvarEtapa') }}" method="POST">
                     @csrf
                     @yield("form-content")
-
                     <input type="hidden" name="etapa" value="@yield('etapa')">
-                    @php
-                        $etapa = intVal($__env->yieldContent('etapa'));
-                    @endphp
-
-                    <!-- Botão Voltar -->
-                    @if ($etapa > 1 && $etapa < 5)
-                        <a href="{{ route('cadastro.cadastro-' . ($etapa - 1)) }}" class="btn-back">Voltar</a>
-                    @endif
-
-                    <!-- Botão Próximo / Concluir -->
-                    @if ($etapa >= 1 && $etapa < 4)
-                        <button type="submit" class="btn-next">Próximo <i class="fa-solid fa-arrow-right"></i></button>
-                    @endif
-
+                    <button type="submit" class="btn-next btn-perfil">Próximo<i class="fa-solid fa-arrow-right"></i></button>
                 </form>
             </div>
             <div class="etapa-atual">
-                @yield('etapa')/3
+                @yield('etapa')/4
+            </div>
+        </div>
+
+        <div class="right-side-container">
+            <div class="background-content">
+                <h1>Faça parte da Avena!</h1>
+                <p>Matenha o progresso sempre visível <br> Dentro e fora do consultório</p>
             </div>
         </div>
 </body>
-</html>
+</html> 

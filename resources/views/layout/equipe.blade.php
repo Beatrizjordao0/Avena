b665d015b2c65d742b8da34c566e9c2bae75ac45
bibiperigosa <beatrizjordao15@gmail.com>
2025-11-23 16:15:42 -0300

    alterações tela de equipe inicial

--- resources/views/layout/equipe.blade.php
+++ resources/views/layout/equipe.blade.php
@@ -14,15 +14,12 @@
     </div>
 
     <div class="acoes-equipe">
-        <button class="btn-ingressar" style="display: inline-flex; width: auto; margin: 0;" onclick="abrirModal()">
-            Ingressar em uma nova equipe
-        </button>
         @if($user->tipo_conta === 'T')
-            <a href="{{ route('equipe.create') }}" class="btn-criarequipe" style="display: inline-flex; width: auto; margin: 0;">Criar nova equipe</a>
+            <a href="{{ route('equipe.create') }}" class="btn-usuario">Criar nova equipe</a>
         @endif
 
         @if($user->tipo_conta === 'P')
-            <button class="btn-ingressar" style="display: inline-flex; width: auto; margin: 0;" onclick="abrirModal()">
+            <button class="btn-usuario" style="display: inline-flex; width: auto; margin: 0;" onclick="abrirModal()">
                 Ingressar em uma nova equipe
             </button>
         @endif
@@ -33,21 +30,45 @@
 <div class="linha-separadora"></div>
 
 <div class="main-teams">
-    @yield('equipe-content')
+    @foreach($equipes as $grupo)
+        <div class="nova-caixa">
+            <div class="linha-conteudo">
+                <div class="container-quadrado">
+                    <div class="mini-quadrado">
+                        @if($user->tipo_conta === 'T')
+                            {{ substr($grupo->nome_gp,0,2) }}
+                        @else
+                            {{ substr($grupo->grupo->nome_gp,0,2) }}
+                        @endif
+                    </div>
+                </div>
+                
+                @if($user->tipo_conta === 'T')
+                    <a href="{{ route('equipe.show', $grupo->id_gp_terapia) }}" class="nome-grupo-link">
+                        <p>{{ $grupo->nome_gp }}</p>
+                    </a>
+                @else
+                    <a href="{{ route('agenda.paciente', $grupo->id_gp) }}" class="nome-grupo-link">
+                        <p>{{ $grupo->grupo->nome_gp }}</p>
+                    </a>
+                @endif
+            </div>
+        </div>
+    @endforeach
 </div>
 
 <!-- MODAL DE INGRESSAR NA EQUIPE -->
 <div id="modal-join" class="modal-overlay" style="display:none;">
     <div class="modal-box">
-        <h2>Ingressar em uma Equipe</h2>
+        <button class="modal-close" onclick="fecharModal()">x</button>
+        <span>Ingresse na sua Equipe de Acompanhamento</span>
+        <p>Insira o código que seu terapeuta te enviou para entrar no seu ambiente de atividades e orientações.</p>
 
-        <form action="{{ route('equipe.join') }}" method="POST">
+        <form class="modal-form" action="{{ route('equipe.join') }}" method="POST">
             @csrf
             <input class="equipe-pesquisa" type="text" name="codigo" placeholder="Digite o código da equipe" required>
             <button type="submit" class="btn-equipes">Entrar</button>
         </form>
-
-        <button class="modal-close" onclick="fecharModal()">Fechar</button>
     </div>
 </div>
 

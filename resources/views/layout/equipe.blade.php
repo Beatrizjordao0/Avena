d78796e2f059800c320919e1436e9ec7e3e10bf3
bibiperigosa <beatrizjordao15@gmail.com>
2025-11-24 00:07:47 -0300

    alterações

--- resources/views/layout/equipe.blade.php
+++ resources/views/layout/equipe.blade.php
@@ -29,32 +29,8 @@
 
 <div class="linha-separadora"></div>
 
-<div class="main-teams">
-    @foreach($equipes as $grupo)
-        <div class="nova-caixa">
-            <div class="linha-conteudo">
-                <div class="container-quadrado">
-                    <div class="mini-quadrado">
-                        @if($user->tipo_conta === 'T')
-                            {{ substr($grupo->nome_gp,0,2) }}
-                        @else
-                            {{ substr($grupo->grupo->nome_gp,0,2) }}
-                        @endif
-                    </div>
-                </div>
-                
-                @if($user->tipo_conta === 'T')
-                    <a href="{{ route('equipe.show', $grupo->id_gp_terapia) }}" class="nome-grupo-link">
-                        <p>{{ $grupo->nome_gp }}</p>
-                    </a>
-                @else
-                    <a href="{{ route('agenda.paciente', $grupo->id_gp) }}" class="nome-grupo-link">
-                        <p>{{ $grupo->grupo->nome_gp }}</p>
-                    </a>
-                @endif
-            </div>
-        </div>
-    @endforeach
+<div class="main-teams">    
+    @yield('equipe-content')
 </div>
 
 <!-- MODAL DE INGRESSAR NA EQUIPE -->
